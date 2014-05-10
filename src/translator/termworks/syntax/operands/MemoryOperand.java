f6b55d011a820c898457e344ec54bce21345ad80
MelnykAndriy <kukoban@ukr.net>
2014-05-11 01:49:39 +0300

    Update MemoryOperand.java

--- src/translator/termworks/syntax/operands/MemoryOperand.java
+++ src/translator/termworks/syntax/operands/MemoryOperand.java
@@ -3,6 +3,7 @@ package translator.termworks.syntax.operands;
 import java.util.ArrayList;
 
 import translator.table.tablecomponents.Atom;
+import translator.table.tablecomponents.Register;
 
 public class MemoryOperand extends Operand {
 	
@@ -14,6 +15,26 @@ public class MemoryOperand extends Operand {
 	public static boolean isMemoryOperand(ArrayList < Atom > operandAtoms) {
 		
 		return false;
+	}
+		
+	public boolean isOffsetPresent() {
+		// TODO
+		return false;
+	}
+	
+	public int getScale() {
+		// TODO
+		return 0;
+	}
+	
+	public Register getBast() {
+		// TODO
+		return null;
+	}
+	
+	public Register getIndex() {
+		// TODO
+		return null;
 	}
 		
 	@Override
